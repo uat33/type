d8fc9d70b663a2fe8577ed77f7c8283db0ec25bc
Uday <101524516+uat33@users.noreply.github.com>
2024-07-12 21:04:12 -0400

    create account improvements

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,13 @@
 const User = require("../models/User");
 const Result = require("../models/Result");
-
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
 
+const Filter = require("bad-words");
+const filter = new Filter();
+const passwordValidator = require("password-validator");
+const schema = new passwordValidator();
+schema.is().min(8).has().uppercase().has().lowercase().has().digits();
 const numSaltRounds = 10;
 
 // @desc get all users
@@ -11,96 +15,110 @@ const numSaltRounds = 10;
 // @access Private
 
 const getAllUsers = asyncHandler(async (req, res) => {
-	const users = await User.find().select("-password").lean();
-	if (!users?.length) {
-		return res.status(400).json({ message: "No users found" });
-	}
+    const users = await User.find().select("-password").lean();
+    if (!users?.length) {
+        return res.status(400).json({ message: "No users found" });
+    }
 
-	res.json(users);
+    res.json(users);
 });
 
 // @desc update a users
 // @route patch /users
 // @access Private
 const createNewUser = asyncHandler(async (req, res) => {
-	const { username, password } = req.body;
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({ message: "All fields required" });
+    }
 
-	if (!username || !password) {
-		return res.status(400).json({ message: "All fields required" });
-	}
+    try {
+        if (filter.isProfane(username)) {
+            return res.status(400).json({ message: "Inappropriate username." });
+        }
 
-	const duplicate = await User.findOne({ username }).lean().exec();
-	if (duplicate) {
-		return res.status(409).json({ message: "Username taken" });
-	}
+        // TODO: password strength
+        // if (!schema.validate(password)) {
+        //     const missing = schema.validate(password, { list: true });
 
-	// hash pw
-	const hashed = await bcrypt.hash(password, numSaltRounds); // 10 salt rounds
+        //     return res.status(400).json({ message: missing });
+        // }
+        const duplicate = await User.findOne({ username }).lean().exec();
+        if (duplicate) {
+            return res.status(409).json({ message: "Username taken" });
+        }
 
-	// create object
-	const userObject = { username, password: hashed };
+        const hashed = await bcrypt.hash(password, numSaltRounds);
 
-	const user = await User.create(userObject);
+        const userObject = { username, password: hashed };
 
-	if (user) {
-		res.status(201).json({ message: `New user ${username} created.` });
-	} else {
-		res.status(400).json({ message: `Invalid user data` });
-	}
+        const user = await User.create(userObject);
+
+        if (user) {
+            return res
+                .status(201)
+                .json({ message: `New user ${username} created.` });
+        } else {
+            return res.status(400).json({ message: `Invalid user data` });
+        }
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
 });
 
 const updateUser = asyncHandler(async (req, res) => {
-	const { id, username, password } = req.body;
-	if (!id || !username || !password) {
-		return res.status(400).json({ message: `All fields required` });
-	}
-	const user = await User.findById(id).exec();
-	if (!user) {
-		return res.status(400).json({ message: `User not found` });
-	}
+    const { id, username, password } = req.body;
+    if (!id || !username || !password) {
+        return res.status(400).json({ message: `All fields required` });
+    }
+    const user = await User.findById(id).exec();
+    if (!user) {
+        return res.status(400).json({ message: `User not found` });
+    }
 
-	const duplicate = await User.findOne({ username }).lean().exec();
+    const duplicate = await User.findOne({ username }).lean().exec();
 
-	if (duplicate && duplicate?._id.toString() !== id) {
-		return res.status(409).json({ message: "duplicate username" });
-	}
+    if (duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({ message: "duplicate username" });
+    }
 
-	user.username = username;
+    user.username = username;
 
-	if (password) {
-		user.password = await bcrypt.hash(password, numSaltRounds);
-	}
+    if (password) {
+        user.password = await bcrypt.hash(password, numSaltRounds);
+    }
 
-	const updatedUser = await user.save();
+    const updatedUser = await user.save();
 
-	res.json({ message: `${updatedUser.username} updated` });
+    res.json({ message: `${updatedUser.username} updated` });
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
-	const { id } = req.body;
+    const { id } = req.body;
 
-	if (!id) {
-		return res.status(400).json({ message: `Id required` });
-	}
+    if (!id) {
+        return res.status(400).json({ message: `Id required` });
+    }
 
-	const posts = await Result.find({ user: "id" }).lean().exec();
+    const posts = await Result.find({ user: "id" }).lean().exec();
 
-	const user = await User.findById(id).exec();
+    const user = await User.findById(id).exec();
 
-	if (!user) {
-		return res.status(400).json({ message: `user not found` });
-	}
+    if (!user) {
+        return res.status(400).json({ message: `user not found` });
+    }
 
-	const result = await user.deleteOne();
-	console.log(result);
+    const result = await user.deleteOne();
+    console.log(result);
 
-	const reply = `Username ${user.username} deleted.`;
-	res.json(reply);
+    const reply = `Username ${user.username} deleted.`;
+    res.json(reply);
 });
 
 module.exports = {
-	getAllUsers,
-	createNewUser,
-	deleteUser,
-	updateUser,
+    getAllUsers,
+    createNewUser,
+    deleteUser,
+    updateUser,
 };
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 connect();
 
 app.use(logger);
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -33,27 +33,27 @@ app.use("/results", resultRoutes);
 app.use("/data", dataRoutes);
 
 app.all("*", (req, res) => {
-	res.status(404);
-	if (req.accepts("html")) {
-		res.sendFile(path.join(__dirname, "views", "404.html"));
-	} else if (req.accepts("json")) {
-		res.json({ message: "404 not found" });
-	} else {
-		res.type("txt").send("404 not found");
-	}
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ message: "404 not found" });
+    } else {
+        res.type("txt").send("404 not found");
+    }
 });
 
 app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
-	console.log("CONNECTED");
-	app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log("CONNECTED");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
 
 mongoose.connection.on("err", (err) => {
-	console.log(err);
-	logEvents(
-		`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-		"mongoErrLog.log"
-	);
+    console.log(err);
+    logEvents(
+        `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+        "mongoErrLog.log"
+    );
 });
