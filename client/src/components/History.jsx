5b0906ea3d4a1b190dc4ec6b939b55e237d93c01
Uday <101524516+uat33@users.noreply.github.com>
2024-07-31 04:07:30 -0400

    Update History.jsx

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -36,7 +36,7 @@ function History() {
                 <h1>Log in to see past results</h1>
             ) : (
                 <div className="max-w-2xl mx-auto p-4 rounded shadow-lg">
-                    <h2 className="text-4xl font-bold mb-4">
+                    <h2 className="text-4xl font-bold mb-4 mt-8">
                         Previous Results
                     </h2>
                     <div className="overflow-x-auto">
